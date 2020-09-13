72b96af3fc52529f79c4618ed554bca18479611f
AlexeyDer <hottelmich@gmail.com>
2020-09-13 16:05:45 +0700

    Add javaDoc for main file

--- src/main/java/shift/Lab/Application.java
+++ src/main/java/shift/Lab/Application.java
@@ -3,6 +3,14 @@ package shift.Lab;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+/**
+ * Класс для запуска программы
+ *
+ * @author "Alexey Derevtsov"
+ */
+
+
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
