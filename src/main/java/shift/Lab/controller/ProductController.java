3b70153136a2d0acf49bf47b9664160b729948bd
AlexeyDer <hottelmich@gmail.com>
2020-09-13 16:02:13 +0700

    Add RestController with get and post mapping

---
+++ src/main/java/shift/Lab/controller/ProductController.java
@@ -0,0 +1,28 @@
+package shift.Lab.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import shift.Lab.entity.Product;
+import shift.Lab.service.ProductService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("product")
+public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping
+    public ResponseEntity<List<Product>> getAllProduct() {
+        return ResponseEntity.ok().body(productService.getAllProduct());
+    }
+
+    @PostMapping
+    public ResponseEntity<Product> createProduct(@RequestBody Product product) {
+        return ResponseEntity.ok().body(this.productService.createProduct(product));
+    }
+
+}
