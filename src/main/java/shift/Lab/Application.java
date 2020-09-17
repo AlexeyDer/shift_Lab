92762bf66839fae702df453b0a9aad2f4c37f43a
AlexeyDer <hottelmich@gmail.com>
2020-09-17 17:45:26 +0700

    Add pc and monitor to db

--- src/main/java/shift/Lab/Application.java
+++ src/main/java/shift/Lab/Application.java
@@ -1,7 +1,14 @@
 package shift.Lab;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import shift.Lab.entity.Monitor;
+import shift.Lab.entity.Pc;
+import shift.Lab.entity.Product;
+import shift.Lab.service.ProductService;
 
 
 /**
@@ -13,7 +20,24 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
+
+    @Autowired
+    private ProductService productService;
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    /**
+     * Метод автоматически создает пользователей в программе
+     */
+
+    @Bean
+    public CommandLineRunner demo() {
+        return (args) -> {
+            productService.createProduct(new Monitor(321, "ASUS", 2.2, 2, "1920x1780"));
+            productService.createProduct(new Pc(12, "HP", 3.3, 5, "neitop"));
+            productService.createProduct(new Pc(123, "HP", 12.3, 3, "desktop"));
+        };
     }
 }
