7e8eea953b9fe803fce922633e9667d148db70c2
AlexeyDer <hottelmich@gmail.com>
2020-09-18 19:20:10 +0700

    Add annotation @Repository

--- src/main/java/shift/Lab/repository/PcRepo.java
+++ src/main/java/shift/Lab/repository/PcRepo.java
@@ -1,8 +1,10 @@
 package shift.Lab.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import shift.Lab.entity.Pc;
 
+@Repository
 public interface PcRepo extends JpaRepository<Pc, Integer> {
 
 }
--- src/main/java/shift/Lab/repository/ProductRepo.java
+++ src/main/java/shift/Lab/repository/ProductRepo.java
@@ -1,8 +1,10 @@
 package shift.Lab.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import shift.Lab.entity.Product;
 
+@Repository
 public interface ProductRepo extends JpaRepository<Product, Integer> {
 
 }
