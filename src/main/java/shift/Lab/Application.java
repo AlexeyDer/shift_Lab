3c9d7c4f9deffaeb427e70c444959989d6e57a39
AlexeyDer <hottelmich@gmail.com>
2020-09-18 19:18:08 +0700

    Add new service to add pc product in db

--- src/main/java/shift/Lab/Application.java
+++ src/main/java/shift/Lab/Application.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Bean;
 import shift.Lab.entity.Monitor;
 import shift.Lab.entity.Pc;
 import shift.Lab.entity.Product;
+import shift.Lab.service.PcService;
 import shift.Lab.service.ProductService;
 
 
@@ -24,6 +25,9 @@ public class Application {
     @Autowired
     private ProductService productService;
 
+    @Autowired
+    private PcService pcService;
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
@@ -36,8 +40,8 @@ public class Application {
     public CommandLineRunner demo() {
         return (args) -> {
             productService.createProduct(new Monitor(321, "ASUS", 2.2, 2, "1920x1780"));
-            productService.createProduct(new Pc(12, "HP", 3.3, 5, "neitop"));
-            productService.createProduct(new Pc(123, "HP", 12.3, 3, "desktop"));
+            pcService.createPc(new Pc(12, "HP", 3.3, 5, "neitop"));
+            pcService.createPc(new Pc(123, "HP", 12.3, 3, "desktop"));
         };
     }
 }
